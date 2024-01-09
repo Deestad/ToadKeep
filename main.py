ecab2c7779114ad041e79baf68928045d8ed7b83
Deestad <kalliddel@gmail.com>
2024-01-08 23:14:00 -0300

    Always on top for windows

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
+import os
 import threading
 import kivy
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.config import Config
+from KivyOnTop import register_topmost, unregister_topmost
 import pyperclip
 import pyautogui
 import pynput
@@ -13,9 +15,11 @@ from pynput import keyboard
 from pynput.keyboard import Key, Controller
 
 # Configuration
+
 Config.set('graphics', 'width', '600')
 Config.set('graphics', 'height', '300')
 Config.set('graphics', 'borderless', '1')
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 kivy.require('2.1.0')
 
 keyboard = Controller()
@@ -28,6 +32,10 @@ class ButtonSet(FloatLayout):
 
 class ToodKeppApp(App):
     def build(self):
+        Window.set_title('Toad Keep')
+        # Make Window Topmost
+        if os.name == "nt":
+            register_topmost(Window, 'Toad Keep')
         self.icon = 'ToadKeep.ico'
         return VirtualKeyboard()
 
