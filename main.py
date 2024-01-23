d694c212805f46984288762f5fad35cebbf43902
Deestad <kalliddel@gmail.com>
2024-01-22 23:11:35 -0300

    Box to type full texts and numbers and copy

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
 from kivy.config import Config
 from kivy.uix.tabbedpanel import TabbedPanel
 
@@ -22,19 +23,22 @@ from pynput.keyboard import Key, Controller
 if os.name == 'nt':
     from KivyOnTop import register_topmost, unregister_topmost
 # Configuration
-with open("ToadKeep.kv", encoding='utf-8') as f:
-    Builder.load_string(f.read())
-
+Config.set('graphics', 'resizable', '0')
 Config.set('graphics', 'width', '600')
 Config.set('graphics', 'height', '300')
 Config.set('graphics', 'borderless', '1')
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 kivy.require('2.1.0')
 
+with open("ToadKeep.kv", encoding='utf-8') as f:
+    Builder.load_string(f.read())
+
+
 keyboard = Controller()
 
 class VirtualKeyboard(TabbedPanel):
     pass
+
 class ButtonSet(FloatLayout):
     pass
 
@@ -48,12 +52,15 @@ class ToodKeppApp(App):
         return VirtualKeyboard()
 
 
+
     def win_build(self):
         # Make Window Topmost
         register_topmost(self, 'Toad Keep')
 
     def callback(self, instance):
         KeyCode = instance.text
+        CopyBox = self.root.ids.copybox
+        CopyBox.text = f"{CopyBox.text}{KeyCode}"
         try:
             pyperclip.copy(KeyCode)
             print(f"{KeyCode} has been copied.")
